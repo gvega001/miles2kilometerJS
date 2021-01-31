2cb3d6bbff7b507a9687b81428190c63c76bff04
Gabriel Vega <gvega001@gmail.com>
2021-01-31 10:46:23 -0800

    Create m2k.js
    m2k javascript file

---
+++ m2k.js
@@ -0,0 +1,8 @@
+let miles =20;
+let miles2kilometers = function(miles){
+    let kilometers =  miles/0.62137;
+    return kilometers;
+}
+let result = miles2kilometers(1);
+console.log( miles + " miles to kilometers: " +result)
+
